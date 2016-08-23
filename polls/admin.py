f5e8b24718911f4b7123ee1070c095a2f9f30841
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-23 14:41:49 -0500

    Add inline choices

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    #This line is for reordering the fields
+
+    #Make subfield group
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+        ]
+    inlines = [ChoiceInLine]
+    #Add inline choices
+
+admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice)
