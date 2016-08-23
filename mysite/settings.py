0868e6fb5672bf235fcd310ef9cea5723c8a23f5
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-23 09:47:08 -0500

    Finished part 2 of the tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    #This is the polls app added
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
