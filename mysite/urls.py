732afd865f076fb227bc2e0bcc84086fe8899a41
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-22 22:15:58 -0500

    Part 1 completed

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from polls import views
+from django.conf.urls import include, url
+from django.contrib import admin
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
 ]
