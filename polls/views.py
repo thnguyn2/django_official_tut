7a78a6cf783f54ffb1bdf78935d925bb6fc52330
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-24 14:03:13 -0500

    Done with part 3 of the tutorial

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from polls.models import Question
+from django.shortcuts import get_object_or_404,render
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello world. You are at the poll index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #Because django has already looked for a folder name /templates/ in each app.
+    context_dict = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context_dict)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
